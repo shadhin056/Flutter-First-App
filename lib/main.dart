de7e6967163cbc366a9d3fe97dfc46ae4bd89192
ShadhiN <shadhinemail@gmail.com>
2019-04-28 10:15:09 +0600

    image view

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(
-          title: Text("First App ShadhiN"),
-        ),
-      ) 
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("First App ShadhiN"),
+      ),
+      body: Card(
+          child: Column(
+        children: <Widget>[
+          Image.asset("assets/food.jpg"),
+          Text("Food Paradise")
+        ],
+      )),
+    ));
   }
 }
