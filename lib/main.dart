5957790353928336fb42114598752b58ece3f02f
ShadhiN <shadhinemail@gmail.com>
2019-04-28 13:42:35 +0600

    add item on button click

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,53 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ["Food Tester"];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("First App ShadhiN"),
-      ),
-      body: Card(
-          child: Column(
-        children: <Widget>[
-          Image.asset("assets/food.jpg"),
-          Text("Food Paradise")
-        ],
-      )),
-    ));
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("First App ShadhiN"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10),
+                child: RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _products.add("ShadhiN");
+                          print(_products);
+                    });
+
+
+                  },
+                  child: Text("Add Product"),
+                ),
+              ),
+              Column(
+                children: _products
+                    .map((element) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset("assets/food.jpg"),
+                              Text(element)
+                            ],
+                          ),
+                        ))
+                    .toList(),
+              ),
+            ],
+          )),
+    );
   }
 }
